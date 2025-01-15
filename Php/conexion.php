cb87100c2f75e167ba9ad5fd2e8a7e830f894781
izeta3 <izanlozlop@gmail.com>
2025-01-15 20:01:16 +0100

    files of newsletter

--- Php/conexion.php
+++ Php/conexion.php
@@ -4,8 +4,8 @@ ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 
 $servername = "127.0.0.1";
-$username = "user";
-$password = "@Mvm2016";
+$username = "izeta3php";
+$password = "Camello@33";
 $dbname = "valoraciones_db";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
---
+++ js/subscribe.js
@@ -0,0 +1,27 @@
+function subscribe() {
+  const email = document.getElementById("email").value;
+
+  if (email === "") {
+    alert("Por favor, ingresa un correo electrónico.");
+    return;
+  }
+
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  if (!emailPattern.test(email)) {
+    alert("Por favor, ingresa un correo electrónico válido.");
+    return;
+  }
+
+  const successMessage = document.getElementById("success-message");
+
+  successMessage.textContent = "Te has suscrito correctamente!";
+  successMessage.classList.add("show");
+
+  document.getElementById("email").value = "";
+
+  setTimeout(function() {
+    successMessage.classList.remove("show");
+  }, 4000);
+}
+
